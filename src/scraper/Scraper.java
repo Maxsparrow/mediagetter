9d0d0ecdd5ae405eada7a3938f12a2cb2712bf89
Maxsparrow <Maxsparrow@gmail.com>
2016-01-10 16:53:25 -0600

    minor refactoring

--- src/scraper/Scraper.java
+++ src/scraper/Scraper.java
@@ -11,7 +11,12 @@ import com.jaunt.*;
 
 public class Scraper {
 
-	String DVD_RELEASE_BASE_URL = "http://www.dvdsreleasedates.com/releases/";
+	private String DVD_RELEASE_BASE_URL = "http://www.dvdsreleasedates.com/releases/";
+	private UserAgent userAgent;
+
+    public Scraper() {
+        userAgent = new UserAgent();
+    }
 
 	public String buildDVDReleaseURL(int year, int month) {
 		DateFormatSymbols dfs = new DateFormatSymbols();
@@ -24,44 +29,49 @@ public class Scraper {
 		return DVDReleaseURL.toString();
 	}
 
+    private Element getTableFromURL(String url, int tableNumber) {
+        try {
+            userAgent.visit(url); // visit
+
+            // Find all the tables
+            Elements tables = userAgent.doc.findEach("<table>");
+            System.out.println("Found " + tables.size() + " tables:");
+            System.out.println("Returning table number " + tableNumber);
+
+            return tables.getElement(tableNumber);
+        }
+        catch (JauntException e){
+            System.out.println(e);
+        }
+    }
+
 	public List<String> findDVDReleases(int year, int month) {
 
 		String DVDReleaseURL = buildDVDReleaseURL(year, month);
 
 		List<String> DVDList = new ArrayList<String>();
 
-		try {
-			UserAgent userAgent = new UserAgent(); // create new userAgent
-													// (headless browser).
-			userAgent.visit(DVDReleaseURL); // visit
+        Element table = getTableFromURL(DVDReleaseURL, 0);
 
-			// Find all the tables
-			Elements tables = userAgent.doc.findEach("<table>");
-			System.out.println("Found " + tables.size() + " tables:");
+        // Loop through rows in first table,
+        Elements trs = table.findEach("<tr>");
+        System.out.println("Found " + trs.size() + " TRs:");
+        for (Element tr : trs) {
+            // Find specific rows with link style for dvd releases
+            Elements links = tr.findEach("<a style='color:#000;'>");
+            for (Element link : links) {
+                String linkText = link.getText();
+                if (linkText.indexOf('[') != -1)
+                    linkText = linkText.substring(0, linkText.indexOf('[') - 1);
 
-			// Loop through rows in first table,
-			System.out.println("Checking first table");
-			Elements trs = tables.getElement(0).findEach("<tr>");
-			System.out.println("Found " + trs.size() + " TRs:");
-			for (Element tr : trs) {
-				// Find specific rows with link style for dvd releases
-				Elements links = tr.findEach("<a style='color:#000;'>");
-				for (Element link : links) {
-					String linkText = link.getText();
-					if (linkText.indexOf('[') != -1)
-						linkText = linkText.substring(0, linkText.indexOf('[') - 1);
+                // Fix special html escape characters
+                linkText = StringEscapeUtils.unescapeHtml4(linkText);
 
-					// Fix special html escape characters
-					linkText = StringEscapeUtils.unescapeHtml4(linkText);
+                DVDList.add(linkText);
+            }
+        }
 
-					DVDList.add(linkText);
-				}
-			}
-		} catch (JauntException e) { // if an HTTP/connection error occurs
-			System.err.println(e);
-		}
-
-		return DVDList;
+        return DVDList;
 	}
 
 	public String findMetacriticScore(String movieName) {
